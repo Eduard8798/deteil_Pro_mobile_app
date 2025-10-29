b842e3f7630275f57620a637a0e98e25522848cd
Eduard8798 <eduard9999997@gmail.com>
2025-10-29 21:30:29 +0200

    add wash

--- client/deteilingPro/src/screens/WashScreen.tsx
+++ client/deteilingPro/src/screens/WashScreen.tsx
@@ -1,7 +1,8 @@
 import React, {FC} from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import {Animated, Image, StyleSheet, Text, View} from "react-native";
 import {RootTabParamList} from "../navigation/MyTabs";
 import {RouteProp} from "@react-navigation/native";
+import ScrollView = Animated.ScrollView;
 
 
 type ProfileScreenRouteProp = RouteProp<RootTabParamList, 'Wash'>;
@@ -13,9 +14,60 @@ interface ProfileScreenProps {
 const WashScreen: FC<ProfileScreenProps> = ({route}) => {
     const userId = route.params?.userId ?? 2;
     return (
-        <View style={styles.container}>
-            <Text>Профиль пользователя ID: {userId !== undefined ? userId: 2}</Text>
-        </View>
+        <ScrollView style={styles.container}
+                    contentContainerStyle={styles.contentContainer}
+        >
+            <Image
+                source={{
+                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/wash_taz3nt.jpg',
+                }}
+                style={styles.image}
+                resizeMode="cover"
+            />
+
+            <Image
+                source={{
+                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/interior_vacuum_cleaner_sw1dlv.jpg',
+                }}
+                style={styles.image}
+                resizeMode="cover"
+            />
+            <Image
+                source={{
+                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765527/toner_zgimzk.jpg',
+                }}
+                style={styles.image}
+                resizeMode="cover"
+            />
+            <Image
+                source={{
+                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/keramic_pynt1o.jpg',
+                }}
+                style={styles.image}
+                resizeMode="cover"
+            />
+            <Image
+                source={{
+                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/gluing_armored_film_lu6tgt.webp',
+                }}
+                style={styles.image}
+                resizeMode="cover"
+            />
+            <Image
+                source={{
+                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/dry_cleaning_uiigbp.webp',
+                }}
+                style={styles.image}
+                resizeMode="cover"
+            />
+            <Image
+                source={{
+                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/1qb33ngtv1t58jfyp2uy6mjn071y7q7c_bbtaiw.jpg',
+                }}
+                style={styles.image}
+                resizeMode="cover"
+            />
+        </ScrollView>
     );
 };
 
@@ -24,7 +76,17 @@ export default WashScreen;
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        justifyContent:'center',
-        alignItems: 'center'
+
+    },
+    contentContainer: {
+        alignItems: 'center',  // теперь тут!
+        paddingVertical: 15,
+    },
+    image: {
+        width: 300,
+        height: 130,
+        borderRadius: 12,
+        margin:5,
+
     }
 })
