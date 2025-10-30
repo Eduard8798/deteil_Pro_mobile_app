90164204442ff08fa69531e17d67e1409224bf97
Eduard8798 <eduard9999997@gmail.com>
2025-10-30 22:01:19 +0200

    photo

--- client/deteilingPro/src/screens/WashScreen.tsx
+++ client/deteilingPro/src/screens/WashScreen.tsx
@@ -1,9 +1,7 @@
 import React, {FC} from 'react';
-import {Animated, Image, StyleSheet, Text, View} from "react-native";
+import {Animated, Image, StyleSheet} from "react-native";
 import {RootTabParamList} from "../navigation/MyTabs";
 import {RouteProp} from "@react-navigation/native";
-import ScrollView = Animated.ScrollView;
-
 
 type ProfileScreenRouteProp = RouteProp<RootTabParamList, 'Wash'>;
 
@@ -12,62 +10,31 @@ interface ProfileScreenProps {
 }
 
 const WashScreen: FC<ProfileScreenProps> = ({route}) => {
-    const userId = route.params?.userId ?? 2;
-    return (
-        <ScrollView style={styles.container}
-                    contentContainerStyle={styles.contentContainer}
-        >
-            <Image
-                source={{
-                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/wash_taz3nt.jpg',
-                }}
-                style={styles.image}
-                resizeMode="cover"
-            />
 
-            <Image
-                source={{
-                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/interior_vacuum_cleaner_sw1dlv.jpg',
-                }}
-                style={styles.image}
-                resizeMode="cover"
-            />
-            <Image
-                source={{
-                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765527/toner_zgimzk.jpg',
-                }}
-                style={styles.image}
-                resizeMode="cover"
-            />
-            <Image
-                source={{
-                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/keramic_pynt1o.jpg',
-                }}
-                style={styles.image}
-                resizeMode="cover"
-            />
-            <Image
-                source={{
-                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/gluing_armored_film_lu6tgt.webp',
-                }}
-                style={styles.image}
-                resizeMode="cover"
-            />
-            <Image
-                source={{
-                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/dry_cleaning_uiigbp.webp',
-                }}
-                style={styles.image}
-                resizeMode="cover"
-            />
-            <Image
-                source={{
-                    uri: 'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/1qb33ngtv1t58jfyp2uy6mjn071y7q7c_bbtaiw.jpg',
-                }}
-                style={styles.image}
-                resizeMode="cover"
-            />
-        </ScrollView>
+
+    return (
+        <Animated.ScrollView
+            style={styles.container}
+            contentContainerStyle={styles.content}
+            showsVerticalScrollIndicator={false}
+        >
+            {[
+                'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/wash_taz3nt.jpg',
+                'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/interior_vacuum_cleaner_sw1dlv.jpg',
+                'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765527/toner_zgimzk.jpg',
+                'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/keramic_pynt1o.jpg',
+                'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/gluing_armored_film_lu6tgt.webp',
+                'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/dry_cleaning_uiigbp.webp',
+                'https://res.cloudinary.com/druvfoz4x/image/upload/v1761765526/1qb33ngtv1t58jfyp2uy6mjn071y7q7c_bbtaiw.jpg',
+            ].map((uri, i) => (
+                <Image
+                    key={i}
+                    source={{ uri }}
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            ))}
+        </Animated.ScrollView>
     );
 };
 
@@ -75,18 +42,17 @@ export default WashScreen;
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-
+        flex: 1,
+        backgroundColor: '#fff',
     },
-    contentContainer: {
-        alignItems: 'center',  // теперь тут!
+    content: {
+        alignItems: 'center',
         paddingVertical: 15,
     },
     image: {
         width: 300,
         height: 130,
         borderRadius: 12,
-        margin:5,
-
-    }
-})
+        marginVertical: 10,
+    },
+});
