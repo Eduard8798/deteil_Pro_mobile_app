5c15b0cc6e45ce661b55d13b73ea094e2e232361
Eduard8798 <eduard9999997@gmail.com>
2025-11-10 22:39:04 +0200

    screen

--- client/deteilingPro/src/screens/ProfileScreen.tsx
+++ client/deteilingPro/src/screens/ProfileScreen.tsx
@@ -14,6 +14,7 @@ import {
 
 const AuthScreen: FC = () => {
     return (
+        // android-?
         <SafeAreaView style={styles.safe}>
             <KeyboardAvoidingView
                 style={styles.flex}
